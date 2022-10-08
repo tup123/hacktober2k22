30b453478e133abc2c41558077dac86d3936f28f
madhu1256 <93447467+madhu1256@users.noreply.github.com>
2022-10-08 12:22:45 +0530

    Create lowest-common-ancestor-of-a-binary-tree.java

---
+++ lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,24 @@
+class Solution {
+    TreeNode ans=null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        helper(root,p,q);
+        return this.ans;
+    }
+    
+    public TreeNode helper(TreeNode node,TreeNode p, TreeNode q){
+        if(node==null){
+            return null;
+        }
+        
+        TreeNode leftNode=helper(node.left,p,q);
+        TreeNode rightNode=helper(node.right,p,q);
+        if(((node==p || node==q)&&(leftNode!=null || rightNode!=null))||(leftNode!=null &&rightNode!=null)){
+            this.ans=node;
+            return node;
+        }else if((leftNode!=null || rightNode!=null)||(node==p || node==q)){
+            return node;
+        }else{
+            return null;
+        }
+    }
+}
